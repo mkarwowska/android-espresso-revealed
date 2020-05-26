5cba6a4efb4dd7fa199ff7d94c68fb659a35b232
Marzena Karwowska (PGS Software) <mkarwowska@pgs-soft.com>
2020-05-26 08:28:23 +0200

    Chapter 5 - exercise 15 change intent implementation

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/test/chapter5/StubSelectImageIntentTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/test/chapter5/StubSelectImageIntentTest.kt
@@ -11,6 +11,7 @@ import com.example.android.architecture.blueprints.todoapp.addedittask.AddEditTa
 import com.example.android.architecture.blueprints.todoapp.tasks.TasksActivity
 import com.example.android.architecture.blueprints.todoapp.test.chapter1.data.TestData
 import com.example.android.architecture.blueprints.todoapp.test.chapter3.*
+import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.CoreMatchers.not
 import org.junit.Before
 import org.junit.Rule
@@ -40,12 +41,12 @@ class StubSelectImageIntentTest {
         toDoDescription = TestData.getToDoDescription()
     }
 
+    //Exercise 15 Change Intent implementation
     @Test
     fun stubsImageIntentWithDrawable() {
         val toDoImage =
-                com.example.android.architecture.blueprints.todoapp.mock.test.R.drawable.todo_image_drawable
-
-        Intents.intending(not(isInternal()))
+               com.example.android.architecture.blueprints.todoapp.mock.test.R.drawable.cookie
+        Intents.intending(hasType("image/*"))
                 .respondWith(IntentHelper.createImageResultFromDrawable(toDoImage))
 
         // Adding new TO-DO.
@@ -59,11 +60,12 @@ class StubSelectImageIntentTest {
         viewWithText(toDoTitle).click()
     }
 
+    //Exercise 15 Change Intent implementation
     @Test
     fun stubsImageIntentWithAsset() {
         val imageFromAssets = "todo_image_assets.png"
 
-        Intents.intending(not(isInternal()))
+        Intents.intending(hasAction(Intent.ACTION_GET_CONTENT))
                 .respondWith(IntentHelper.createImageResultFromAssets(imageFromAssets))
 
         // Adding new TO-DO.
