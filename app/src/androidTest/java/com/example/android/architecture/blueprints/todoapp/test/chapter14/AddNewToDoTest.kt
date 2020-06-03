f19b11a8b41c5240363e0126ad449c5a36c7e77d
Marzena Karwowska (PGS Software) <mkarwowska@pgs-soft.com>
2020-06-03 14:44:15 +0200

    Chapter 14 - exercises 29.2-4

---
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/test/chapter14/AddNewToDoTest.kt
@@ -0,0 +1,36 @@
+package com.example.android.architecture.blueprints.todoapp.test.chapter14
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.tasks.TasksActivity
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class AddNewToDoTest {
+
+    @Before
+    fun launchTasksActivity() {
+        ActivityScenario.launch(TasksActivity::class.java)
+    }
+
+    @Test
+    fun addsNewToDo() {
+        // adding new TO-DO
+        onView(withId(R.id.fab_add_task)).perform(click())
+        onView(withId(R.id.add_task_title))
+                .perform(ViewActions.typeText("Example title"), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.fab_edit_task_done)).perform(click())
+        // verifying new TO-DO with title is shown in the TO-DO list
+        onView(withText("Example title")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+}
---
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/test/chapter14/examplesForFailTest.kt
@@ -0,0 +1,42 @@
+package com.example.android.architecture.blueprints.todoapp.test.chapter14
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiSelector
+import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.tasks.TasksActivity
+import org.junit.Assert.assertFalse
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class examplesForFailTest {
+
+    private val uiDevice: UiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+
+    @Before
+    fun launchTasksActivity() {
+        ActivityScenario.launch(TasksActivity::class.java)
+    }
+
+    @Test
+    fun exampleTestToGenerateJunitAssertionErros() {
+        onView(ViewMatchers.withId(R.id.fab_add_task)).perform(ViewActions.click())
+        val selector = uiDevice.findObject(UiSelector().resourceId("com.example.android.architecture.blueprints.todoapp.mock:id/add_task_title"))
+        assertFalse("element with selector $selector is present on the screen when it should not", selector.exists())
+    }
+
+    @Test
+    fun exampleTestToGenerateTruthAssertionErros() {
+        onView(ViewMatchers.withId(R.id.fab_add_task)).perform(ViewActions.click())
+        val selector = uiDevice.findObject(UiSelector().resourceId("com.example.android.architecture.blueprints.todoapp.mock:id/add_task_title"))
+        assertThat(selector.exists()).isFalse()
+    }
+}
